6b54aa37d9182abff212af89653c37d1098324be
Adrian M. <adidouuu7@gmail.com>
2024-05-22 12:30:32 +0200

    Seance prpojet

--- Tests/scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const contactForm = document.getElementById('contactForm');
-    contactForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        alert('Merci pour votre message ! Nous vous répondrons bientôt.');
-        contactForm.reset();
-    });
-});
