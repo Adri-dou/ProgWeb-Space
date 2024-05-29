04c442d0030b3b9b5007b7d37504771efcb6b4d8
Adrian M. <adidouuu7@gmail.com>
2024-05-29 12:29:26 +0200

    apod api

--- js/scripts.js
+++ js/scripts.js
@@ -65,3 +65,51 @@ function displayAstreInfo(data) {
         cell2.innerHTML = prop.value;
     });
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const apiKey = 'DEMO_KEY';  // Remplacez 'DEMO_KEY' par votre clé API de la NASA
+    const apodContainer = document.getElementById('apod-api');
+
+    async function fetchApodData(date) {
+        const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`);
+        const data = await response.json();
+        return data;
+    }
+
+    async function fetchLastThreeApods() {
+        const dates = [];
+        for (let i = 0; i < 3; i++) {
+            const date = new Date();
+            date.setDate(date.getDate() - i);
+            const formattedDate = date.toISOString().split('T')[0];
+            dates.push(formattedDate);
+        }
+
+        const apodDataPromises = dates.map(date => fetchApodData(date));
+        const apodData = await Promise.all(apodDataPromises);
+
+        apodData.forEach(data => {
+            const apodItem = document.createElement('div');
+            apodItem.className = 'apod-item';
+            if (data.media_type === 'image') {
+                apodItem.innerHTML = `
+                    <img src="${data.url}" alt="${data.title}">
+                    <p>${data.title}</p>
+                `;
+            } else if (data.media_type === 'video') {
+                apodItem.innerHTML = `
+                    <iframe src="${data.url}" frameborder="0" allowfullscreen></iframe>
+                    <p>${data.title}</p>
+                `;
+            } else {
+                apodItem.innerHTML = `
+                    <p>Contenu multimédia non pris en charge</p>
+                `;
+            }
+            apodContainer.appendChild(apodItem);
+        });
+    }
+
+    fetchLastThreeApods();
+});
+
